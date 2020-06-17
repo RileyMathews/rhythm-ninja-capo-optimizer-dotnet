7d25662c87b9c3e67b9e7838c3021d67fc47411f
Riley Mathews <rileymathews80@gmail.com>
2020-06-17 17:57:26 -0500

    fixed errant code style

--- MusicLib/services/CapoService.cs
+++ MusicLib/services/CapoService.cs
@@ -5,16 +5,16 @@ namespace MusicLib
 {
   public class CapoService
   {
-    public Song originalSong;
+    public Song OriginalSong;
 
     public CapoService(Song originalSong)
     {
-      this.originalSong = originalSong;
+      this.OriginalSong = originalSong;
     }
     public List<Transposition> Optimize()
     {
       List<int> seeds = new List<int>() { 1, 2, 3, 4, 5 };
-      List<Transposition> transpositions = seeds.Select(i => new Transposition(originalSong, i)).ToList();
+      List<Transposition> transpositions = seeds.Select(i => new Transposition(OriginalSong, i)).ToList();
       List<Transposition> transpositionsWithCagedKeys = transpositions.Where(i => i.NewSong.Keys.Any(key => key.IsCagedKey())).ToList();
       List<Transposition> transpositionsByCagedCount = transpositionsWithCagedKeys.OrderByDescending(i => i.NewSong.Keys.Count(key => key.IsCagedKey())).ToList();
       return transpositionsByCagedCount;
